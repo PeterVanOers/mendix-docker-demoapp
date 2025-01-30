749feef9b4c8783479573a435cc027cd45a64261
Peter van Oers <37449405+PeterVanOers@users.noreply.github.com>
2025-01-30 09:45:49 +0100

    Add files via upload

--- connect.sh
+++ connect.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker exec -it firstapp_mx_app su mendix -P -c "cd; /bin/bash"
+docker exec -it demoapp_mx_app su mendix -P -c "cd; /bin/bash"
--- copy.sh
+++ copy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker exec firstapp_mx_app bash -c "rm /srv/mendix/data/model-upload/*"
-docker cp $1 firstapp_mx_app:/srv/mendix/data/model-upload/mendix.mda
-docker exec firstapp_mx_app bash -c "chown mendix:mendix /srv/mendix/data/model-upload/mendix.mda"
-docker exec firstapp_mx_app bash -c "chmod 700 /srv/mendix/data/model-upload/mendix.mda"
-docker exec firstapp_mx_app bash -c "ls -al /srv/mendix/data/model-upload/"
+docker exec demoapp_mx_app bash -c "rm /srv/mendix/data/model-upload/*"
+docker cp $1 demoapp_mx_app:/srv/mendix/data/model-upload/mendix.mda
+docker exec demoapp_mx_app bash -c "chown mendix:mendix /srv/mendix/data/model-upload/mendix.mda"
+docker exec demoapp_mx_app bash -c "chmod 700 /srv/mendix/data/model-upload/mendix.mda"
+docker exec demoapp_mx_app bash -c "ls -al /srv/mendix/data/model-upload/"
